115cf92aa13d8743a25436d321f4daaae5148c32
rafi4482 <rafi.kazla@gmail.com>
2024-07-17 15:49:50 +0600

    UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <p>Follow us on social media:</p>
+      <div className="social-links">
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg"
+            alt="Facebook"
+          />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://cdn-icons-png.freepik.com/256/124/124021.png?semt=ais_hybrid"
+            alt="Twitter"
+          />
+        </a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://img.freepik.com/premium-vector/instagram-vector-logo-icon-social-media-logotype_901408-390.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1721088000&semt=ais_user"
+            alt="Instagram"
+          />
+        </a>
+      </div>
+      <p>&copy; 2024 Pizza Shop. All rights reserved.</p>
+    </footer>
+  );
 };
 
 export default Footer;
